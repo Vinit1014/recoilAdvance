634a0992ffcd6c7d2c1bce194754086906e3984e
Vinit <vinits1014@gmail.com>
2024-12-14 11:55:27 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+import { useRecoilValue, RecoilRoot, useRecoilState } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
+
+function App() {
+  return <RecoilRoot>
+    <MainApp/>
+  </RecoilRoot>
+  
+}
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationsAtomCount = useRecoilValue(notificationsAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+  return (
+    <>
+      <button>Home</button>
+      <button>My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs ({jobsAtomCount})</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notifications ({notificationsAtomCount})</button>
+ 
+      <button>Me ({totalNotificationCount})</button>
+
+    </>
+  )
+}
+
+
+
+export default App
